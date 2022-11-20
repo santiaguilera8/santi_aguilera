ba45110c3296c6dbfeff80c932e6009e8efd4a18
santiaguilera8 <santiipashion@gmail.com>
2022-11-20 19:23:18 -0300

    Tarea clase 9

--- Semana 5/Clase 9/js/script.js
+++ Semana 5/Clase 9/js/script.js
@@ -1,33 +1,10 @@
-const dato1 = parseInt(prompt("Ingrese un numero"));
-const dato2 = parseInt(prompt("Ingrese otro numero"));
-const dato3 = parseInt(prompt("Ingrese otro numero"));
+let pais = "Argentina ";
+let continente = " Latinoamérica";
+let antiguedad = "2022";
+let pais_y_continente = pais + "" + continente;
 
-if (dato1 == dato2 && dato1 == dato3) {
-   document.write("no seas boludo, cambia los datos");
-} 
- else {
-    if (dato1>dato2) {
-        if (dato1>dato3) {
-            document.write("El primer número ingresado es el más grande");
-        } else {
-            document.write("El tercer número ingresado es el más grande");
-        }
-    } else {
-        if (dato2>dato3) {
-            document.write("El segundo número ingresado es el más grande");
-        } else {
-            document.write("El tercer número ingresado es el más grande");
-        }
-    }
-}
+pais = "Uruguay";
+continente = "Latinoamérica";
 
-
-// 
-
-const num = parseInt(prompt("Ingrese un numero"));
-
-if (num % 2 ==0){
-    document.write("El número es par")
-} else{
-    document.write("El número")
-}
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
